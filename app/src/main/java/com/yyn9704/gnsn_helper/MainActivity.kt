fc0bb94341cc63e4387f3a92e694084c6dc5369b
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-11-28 23:44:21 +0900

    initial commit

---
+++ app/src/main/java/com/yyn9704/gnsn_helper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yyn9704.gnsn_helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
